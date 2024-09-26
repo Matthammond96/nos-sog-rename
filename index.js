bf0b279bb9d2314ced0a53cd434313056e4f7a06
Matthew Hammond <ultramatt96@hotmail.co.uk>
2024-09-26 20:45:07 +0100

    chore: fix controlnets

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const dbPath = relativePath("../../../../..//.nosana/nosana_db.json");
 var db = JSON.parse(fs.readFileSync(dbPath, "utf8"));
 
 db.resources.volumes[
-  "https://pub-5bc58981af9f42659ff8ada57bfea92c.r2.dev/controlnet"
+  "https://pub-5bc58981af9f42659ff8ada57bfea92c.r2.dev/controlnets"
 ] = db.resources.volumes["https://safetensor.sogni.ai/controlnets"];
 db.resources.volumes[
   "https://pub-5bc58981af9f42659ff8ada57bfea92c.r2.dev/Sogni%20Supported%20SD1.5-https://pub-5bc58981af9f42659ff8ada57bfea92c.r2.dev/Sogni%20Supported%20SDXL-https://safetensor.sogni.ai/Sogni+Supported+SD1.5-https://safetensor.sogni.ai/Sogni+Supported+SDXL"
